40d52d16c1b9a0c5a6f3565a24d24c84e2f20c28
mrabet2002 <mrabetahmed1807@gmail.com>
2024-04-08 17:15:51 +0000

    Edit product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { NewProductsComponent } from './components/new-products/new-products.component';
 import { HomeComponent } from './components/home/home.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
 
 const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'new-products', component: NewProductsComponent },
   { path: '', component: HomeComponent },
-  { path: 'edit-product/:id', component: NewProductsComponent }
+  { path: 'edit-product/:id', component: EditProductComponent }
 ];
 
 @NgModule({
--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -1,35 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Product } from '../../models/Product';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.css'
 })
-export class EditProductComponent {
+export class EditProductComponent implements OnInit {
   productForm!: FormGroup<any>;
   message: any;
   error: any;
-  product!: Product;
+  productId!: string;
 
   constructor(private productService: ProductsService,
     private router: Router
-  ) { }
+  ) {
+    
+   }
 
   ngOnInit() {
     //Initialize the form
-    this.productForm = new FormGroup({
-      name: new FormControl('', Validators.required),
-      price: new FormControl('', Validators.required),
-      checked: new FormControl(false)
-    });
+      this.productForm = new FormGroup({
+        name: new FormControl('', Validators.required),
+        price: new FormControl('', Validators.required),
+        checked: new FormControl(false)
+      });
+    
 
-    let productId:string = this.router.routerState.snapshot.url.split('/').pop()!;
-    this.productService.getProduct(productId).subscribe((product) => {
-      this.product = product;
+    this.productId = this.router.routerState.snapshot.url.split('/').pop()!;
+    
+    this.productService.getProduct(this.productId).subscribe((product) => {
       this.productForm.setValue({
         name: product.name,
         price: product.price,
@@ -45,11 +47,10 @@ export class EditProductComponent {
       return;
     }
 
-    this.productService.updateProduct(this.productForm.value, this.product.id).subscribe({
+    this.productService.updateProduct(this.productForm.value, this.productId).subscribe({
       next: response => {
         this.error = "";
         this.message = "Product added successfully!";
-        this.productForm.reset();
       },
       error: error => {
         this.message = "";
